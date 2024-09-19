b1c43b416e940d9d4c4aa2b016accdafd3a4301a
XaramX <52756403+Aram-schoolboy@users.noreply.github.com>
2024-09-19 14:17:57 +0300

    Update pile.cpp

--- lib/source/pile.cpp
+++ lib/source/pile.cpp
@@ -2,11 +2,8 @@
 #include "../headers/path_constructor.h"
 #include "../headers/bmp.h"
 
-SandPileModel::SandPileModel(Grid* grid, long long freq, long long max_iter, const char* output) {
-    this->grid = grid;
-    this->max_iter = max_iter;
-    this->output = output;
-    this->freq = freq;
+SandPileModel::SandPileModel(Grid* grid, long long freq, long long max_iter, const char* output):
+                            grid(grid), max_iter(max_iter), output(output), freq(freq) {
     if (freq == 0) {
         this->freq = max_iter + 1;
     }
@@ -15,7 +12,7 @@ SandPileModel::SandPileModel(Grid* grid, long long freq, long long max_iter, con
 void SandPileModel::LaunchCollapse() const {
     long long iter_count = 0;
     while ((iter_count < max_iter) && ((*grid).MakeNaiveIteration())) {
-        iter_count++;
+        ++iter_count;
         if (iter_count % freq == 0) {
             char full_path[kMaxOutputLength] {};
             MakeFullPath(full_path, output, iter_count);
